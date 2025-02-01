3bf97864d28bc4a889747fd87d574c6fe4b425bb
Law-Chun-Man <126952896+Law-Chun-Man@users.noreply.github.com>
2025-02-01 08:47:57 +0800

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -3,14 +3,7 @@ xmodmap .Xmodmap
 xfce4-clipman &
 /usr/bin/sudo tlp start
 blueman-applet &
-#picom --backend glx --vsync &
 picom --backend glx --vsync --corner-radius 10 --inactive-opacity 0.8 &
 /usr/lib/x86_64-linux-gnu/libexec/kdeconnectd &
-xrandr --output DisplayPort-1 --same-as eDP --mode 1920x1080
 nm-applet &
 xset s 600
-/bin/bash -c "sudo rfkill unblock bluetooth"
-#/usr/lib/x86_64-linux-gnu/libexec/kdeconnectd
-#echo schedutil | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-#ibus start &
-#rofi -show run
