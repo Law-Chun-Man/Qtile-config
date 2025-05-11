59aeeb233643045020ff90c2a02a9234843b5d38
Law-Chun-Man <126952896+Law-Chun-Man@users.noreply.github.com>
2025-05-11 19:42:57 +0800

    Delete dunst directory

--- dunst/brightness_increase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-brightnessctl set +1%
-
-BRIGHTNESS=$(brightnessctl get)
-
-dunstify -r 1235 -h int:value:"$BRIGHTNESS" -I ~/.config/qtile/dunst/display-brightness.png -t 3000 "$BRIGHTNESS%"
-
