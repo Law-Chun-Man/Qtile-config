8baa77ab057fcbbab1816f263a3482e5264d70cc
Law-Chun-Man <126952896+Law-Chun-Man@users.noreply.github.com>
2025-05-11 19:42:37 +0800

    Delete bash_script directory

--- bash_script/cpu_temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-temp_milli=$(cat "/sys/class/thermal/thermal_zone0/temp")
-
-temp_celsius=$((temp_milli / 1000))
-
-echo "${temp_celsius:0:2}°C"
-
--- bash_script/time.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-date +"%H:%M:%S"
-
